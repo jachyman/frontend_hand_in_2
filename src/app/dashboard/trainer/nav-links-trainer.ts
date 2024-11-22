21579e48f4c5cdcb7c98d08ef5926ee8ec71094e
Ena Salaj <enasalaj@gmail.com>
2024-11-22 15:26:02 +0100

    adding ui for users table and adding the table to /dashboard/manager/users

---
+++ src/app/dashboard/trainer/nav-links-trainer.ts
@@ -0,0 +1,8 @@
+export const trainerLinks = [
+    { name: 'Workout plans', href: '/dashboard/client/workout-plan' },
+    { name: 'Users', href: '/dashboard/client/users' },
+    { name: 'Profile', href: '/dashboard/client/profile' }, // change this to dashboard/profile leave for now 
+    { name: 'Log out', href: '/' },
+  ];
+
+  
---
+++ src/app/ui/dashboard/users-listing.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+interface User {
+  id: string;
+  name: string;
+  surname: string;
+}
+
+interface UserListProps {
+  users: User[];
+}
+
+export default function UserList({ users }: UserListProps) {
+  return (
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full max-w-4xl mx-auto border border-gray-200">
+
+      {/* Table */}
+      <div className="overflow-y-auto max-h-80">
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="bg-gray-100 text-left">
+              <th className="py-4 px-6 text-sm font-bold text-gray-600 uppercase tracking-wider">
+                User ID
+              </th>
+              <th className="py-4 px-6 text-sm font-bold text-gray-600 uppercase tracking-wider">
+                Name
+              </th>
+              <th className="py-4 px-6 text-sm font-bold text-gray-600 uppercase tracking-wider">
+                Surname
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr
+                key={user.id}
+                className={`hover:bg-blue-50 transition duration-150 ease-in-out ${
+                  index % 2 === 0 ? "bg-gray-50" : "bg-white"
+                }`}
+              >
+                <td className="py-4 px-6 text-sm text-gray-700 font-medium">
+                  {user.id}
+                </td>
+                <td className="py-4 px-6 text-sm text-gray-700">{user.name}</td>
+                <td className="py-4 px-6 text-sm text-gray-700">
+                  {user.surname}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
