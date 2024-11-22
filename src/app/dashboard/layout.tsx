5ce01da4541f1dd1f5f9fe7badcf7a742004eb43
Ena Salaj <enasalaj@gmail.com>
2024-11-22 15:04:43 +0100

    adding links for trainer dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import TopBar from '@/app/ui/dashboard/top-bar';
 import { usePathname } from 'next/navigation';
 import { managerLinks } from '@/app/dashboard/manager/nav-links-manager';
 import { clientLinks } from '@/app/dashboard/clients/nav-links-client';
+import { trainerLinks } from '@/app/dashboard/trainer/nav-links-trainer';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   // Determine user role and provide the right links dynamically
@@ -24,9 +25,12 @@ function getUserLinks() {
   if (pathname.startsWith('/dashboard/manager')) {
     return managerLinks; // Import these from `nav-links-manager`
   } 
-  if(pathname.startsWith('/dashboard/client')){
+  else if(pathname.startsWith('/dashboard/client')){
     return clientLinks;
   }
-  // do the same for client, personal trainer etc... 
+  else if(pathname.startsWith('/dashboard/trainer')){
+    return trainerLinks;
+  }
+  
   return [];
 }
---
+++ src/app/dashboard/trainer/page.tsx
@@ -0,0 +1,9 @@
+export default function TrainerDashboard() {
+    return (
+      <div>
+        <h1>Welcome to the client dashboard</h1>
+        <p>This is the main content area.</p>
+      </div>
+    );
+  }
+  
