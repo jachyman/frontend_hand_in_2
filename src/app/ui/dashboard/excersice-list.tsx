9e4d73cc6c4d23df903037b91a97289e3d0995d2
jachyman <55025878+jachyman@users.noreply.github.com>
2024-12-03 01:13:51 +0100

    fix table html error in excercise list

--- src/app/ui/dashboard/excersice-list.tsx
+++ src/app/ui/dashboard/excersice-list.tsx
@@ -11,21 +11,23 @@ export default function ExcercisesList({
     return (
         <table style={styles.table}>
             <thead>
-                <th style={{ border: '2px solid black', padding: '8px', width: '30%' }}>Name</th>
-                <th style={{ border: '2px solid black', padding: '8px', width: '50%' }}>Description</th>
-                <th style={{ border: '2px solid black', padding: '8px', width: '10%' }}>Sets</th>
-                <th style={{ border: '2px solid black', padding: '8px', width: '10%' }}>Reps/Time</th>
+                <tr>
+                    <th style={{ border: '2px solid black', padding: '8px', width: '30%' }}>Name</th>
+                    <th style={{ border: '2px solid black', padding: '8px', width: '50%' }}>Description</th>
+                    <th style={{ border: '2px solid black', padding: '8px', width: '10%' }}>Sets</th>
+                    <th style={{ border: '2px solid black', padding: '8px', width: '10%' }}>Reps/Time</th>
+                </tr>
             </thead>
             <tbody>
-                {exercises.map((exercise) => (
-                    <tr key={exercise.exerciseId}>
-                        <td style={styles.cell}>{exercise.name}</td>
-                        <td style={styles.cell}>{exercise.description}</td>
-                        <td style={styles.cell}>{exercise.sets}</td>
-                        <td style={styles.cell}>
-                            {exercise.repetitions !== 0 ? exercise.repetitions : exercise.time || 'ERR'}</td>
-                    </tr>
-                ))}
+            {exercises.map((exercise) => (
+                <tr key={exercise.exerciseId}>
+                    <td style={styles.cell}>{exercise.name}</td>
+                    <td style={styles.cell}>{exercise.description}</td>
+                    <td style={styles.cell}>{exercise.sets}</td>
+                    <td style={styles.cell}>
+                        {exercise.repetitions !== 0 ? exercise.repetitions : exercise.time || 'ERR'}</td>
+                </tr>
+            ))} 
             </tbody>
         </table>
         )
